d41b2a09a8d6595d32c905490ab2a8fde7541afd
shinjune <47058441+shinjjune@users.noreply.github.com>
2019-10-16 08:54:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session=require('express-session');
 var ejsLocals = require('ejs-locals'); // ejs-locals 사용
-
-var sequelize = require('./models').sequelize
+var session = require('express-session');
 
 var app = express();
-sequelize.sync();
 
 // view engine setup
 app.engine('ejs', ejsLocals); // view engine 설정
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('aa'));
 app.use(session({resave:false,
   saveUninitialized:true,
@@ -27,28 +28,20 @@ app.use(session({resave:false,
   }
 }));
 
-
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use('/', require('./routes/index'));
 app.use('/user', require('./routes/userRouter'));
 app.use('/signup', require('./routes/userRouter'));
 app.use('/login', require('./routes/userRouter'));
+app.use('/logout', require('./routes/userRouter'));
 app.use('/findid', require('./routes/userRouter'));
 app.use('/findpw', require('./routes/userRouter'));
 
 app.use('/dop', require('./routes/dopRouter')); // .../dop url 접속 처리
 app.use('/admin',require('./routes/dopAdmin'));
-app.use('/ticket',require('./routes/dopAdmin'));
 
-app.use('/boardReq', require('./routes/boardreq'));
 app.use('/board',require('./routes/boardRouter'));
 app.use('/mission', require('./routes/mission'));
-app.use
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
